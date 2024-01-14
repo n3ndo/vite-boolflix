710770760e62d05d3d8acc33f04f2ab19ae2e854
n3ndo <ferdinandof3lixbagnale@gmail.com>
2024-01-14 20:52:53 +0100

    [UPDATE] added logo, search bar, search button and basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+import { store } from './store.js';
 export default {
-  
+  components:{
+    AppHeader
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    getMoviesAndSeries() {
+      let apiUrl = store.endpoint;
+      if(store.search !== '') {
+        apiUrl += `?api_key=${store.api_key}&query=${store.search}`;
+      }
+      axios.get(apiUrl).then((response) => {
+        store.movies = response.data.data;
+        console.log(store.movies);
+      })
+    }
+  },
 }
 </script>
 <template lang="">
-  <div>
-    
-  </div>
+  <body>
+    <AppHeader @search="getMoviesAndSeries"/>
+  </body>
 </template>
-<style lang="">
-  
+<style lang="scss">
+  @use './styles/generals.scss';
+
+  body {
+    background-color: rgb(67, 67, 67);
+  }
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,44 @@
 <script>
+import axios from 'axios';
+import { store } from '../store.js';
 export default {
-    
+    data() {
+        return {
+            store
+        }
+    }
 }
 </script>
 <template lang="">
-    <div>
-        
-    </div>
+    <header>
+        <div class="container-fluid">
+            <div class="row justify-content-between">
+                <div class="col-4 p-2">
+                    <h1>BOOLFIX</h1>
+                </div>
+                <div class="col-3 p-2 d-flex align-items-center">
+                    <input type="text" class="form-control" placeholder="Cerca..." v-model="store.search" id="search">
+                    <button class="btn btn-primary" @click="$emit('search')">Cerca</button>
+                </div>
+            </div>
+        </div>
+    </header>
 </template>
-<style lang="">
-    
+<style lang="scss" scoped>
+@use '../styles/generals.scss';
+
+header {
+    display: flex;
+    flex-wrap: wrap;
+    height: 75px;
+    background-color: black;
+
+
+    h1 {
+        color: red;
+        font-size: 50px;
+    }
+
+
+}
 </style>

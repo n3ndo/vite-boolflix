67a2e50999ef24e6793451b644f494c8cae01bbd
n3ndo <ferdinandof3lixbagnale@gmail.com>
2024-01-15 23:19:15 +0100

    [UPDATE] Ended MILESTONE 2

--- src/components/MediaCard.vue
+++ src/components/MediaCard.vue
@@ -1,18 +1,28 @@
 <script>
+import { store } from '../store.js';
 export default {
     props: {
         media: Object
     },
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+
+    }
 }
 </script>
 <template lang="">
     <div class="text-white ">
-        <div class="conteiner-fluid">
+        <div class="container-fluid">
             <div >
                 <h2>{{media.original_title}}</h2>
                 <h2>{{media.title}}</h2>
                 <h2>{{media.vote}}</h2>
-                <span>{{media.original_language}}</span>
+                <span :class="{'fi': true, ['fi-' + media.original_language]: true}">
+                </span>
             </div>
         </div>
     </div>
@@ -20,14 +30,8 @@ export default {
 <style lang="scss" scoped>
 @use '../styles/generals.scss';
 
-.content {
-
-
-    h2 {
-        color: red;
-        display: inline-block;
-    }
-
+span{
+    font-size: 25px;
 }
 </style>
 
--- src/store.js
+++ src/store.js
@@ -7,4 +7,10 @@ export const store = reactive({
     series : [],
     search : '',
     api_key : '50e720e23ae0f616a0d8a6ba8bcdeb7e',
+    flags: {
+        'it': 'url(./public/Flags_of_italy.svg.png)',
+        'en': 'url(./public/inghilterra.png)',
+        'es': 'url(./public/spagna.png)',
+        'fr': 'url(./public/Flags_of_France.svg.png)',
+    }
 })
